25441067b7581a5d43319931199f35a42dd08317
SAG <andrschwartz@gmail.com>
2014-02-13 15:11:18 +0200

    Добавил поле ПодОтдел для модели Комнаты

---
+++ smyt_test/migrations/0002_auto__add_field_rooms_subdepartment.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Rooms.subdepartment'
+        db.add_column(u'smyt_test_rooms', 'subdepartment',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=255),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Rooms.subdepartment'
+        db.delete_column(u'smyt_test_rooms', 'subdepartment')
+
+
+    models = {
+        'smyt_test.rooms': {
+            'Meta': {'object_name': 'Rooms'},
+            'department': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'spots': ('django.db.models.fields.IntegerField', [], {}),
+            'subdepartment': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'smyt_test.users': {
+            'Meta': {'object_name': 'Users'},
+            'date_joined': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'paycheck': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['smyt_test']
