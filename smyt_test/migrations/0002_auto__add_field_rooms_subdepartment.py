8710868b47a8f9890bc2be4df5673f720f3a777f
SAG <andrschwartz@gmail.com>
2014-02-13 18:53:30 +0200

    Исправил значение по умолчанию в миграции. Судя по всему, недоработка в валидаторе South. sqlite не принимает пустую строку в качестве дефолтного значения

--- smyt_test/migrations/0002_auto__add_field_rooms_subdepartment.py
+++ smyt_test/migrations/0002_auto__add_field_rooms_subdepartment.py
@@ -10,7 +10,7 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding field 'Rooms.subdepartment'
         db.add_column(u'smyt_test_rooms', 'subdepartment',
-                      self.gf('django.db.models.fields.CharField')(default='', max_length=255),
+                      self.gf('django.db.models.fields.CharField')(default=' ', max_length=255),
                       keep_default=False)
 
 
