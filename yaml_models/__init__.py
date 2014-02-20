8454dadd111efc8c608feac399a3054703c43e2c
SAG <andrschwartz@gmail.com>
2014-02-20 17:04:09 +0200

    Переделал преобразование YAML в классы моделей. Убрал OrderedDictYAMLLoader, вместо этого использовал OrderedDict в dict_to_model_class

--- yaml_models/__init__.py
+++ yaml_models/__init__.py
@@ -4,13 +4,15 @@ import yaml
 from yaml_models.models import YamlModel
 from django.db import models
 from django.contrib import admin as django_admin
-from yaml_models.yaml_omap import OrderedDictYAMLLoader
+from collections import OrderedDict
+
 
 FIELD_TYPE_MAPPING = {
-    'char': {'class': models.CharField, 'initial_kwargs': {'max_length':255}},
+    'char': {'class': models.CharField, 'initial_kwargs': {'max_length': 255}},
     'int': {'class': models.IntegerField, 'initial_kwargs': {}},
     'date': {'class': models.DateField, 'initial_kwargs': {}},
 }
+
 
 def dict_to_model_class(app_name, class_name, model_dict):
     """Преобразует словарь полученный из YAML файла в класс модели"""
@@ -19,7 +21,7 @@ def dict_to_model_class(app_name, class_name, model_dict):
     Meta.verbose_name = model_dict['title']
     Meta.verbose_name_plural = model_dict['title']
     Meta.app_label = app_name
-    fields = {}
+    fields = OrderedDict()
     fields['__module__'] = app_name
     fields['Meta'] = Meta
     for row in model_dict['fields']:
@@ -34,7 +36,7 @@ def dict_to_model_class(app_name, class_name, model_dict):
 
 def models_from_yaml(yaml_path, app_name, models_module, admin_register=True):
     models_module = import_module(models_module)
-    models_data = yaml.load(open(yaml_path), Loader=OrderedDictYAMLLoader)
+    models_data = yaml.load(open(yaml_path))
     result = []
     for key in models_data.keys():
         class_name = key.lower().capitalize()
